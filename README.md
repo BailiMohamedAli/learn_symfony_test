5a688fd8ec258e5ea14cfdf27f388c4ba2b42365
BailiMohamedAli <bailimohamedali@gmail.com>
2021-05-18 17:44:22 +0100

    learning php more and symfony 5 framework

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # learn_symfony_test
 learning the basics of the symfony framework
+## find the link to this tutorial here :
+https://www.youtube.com/watch?v=4t3fNkGwRWo&t=4054s&ab_channel=LiorCHAMLA
+
---
+++ src/Controller/TestController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TestController 
+{
+    public function index() : Response
+    {   
+        #dump() replaces var_dump() :
+            #when in symfony use dump insted of var_dump cause of circular references Errors
+                #dd() replaces dump() die()
+                #dump("it's alive !");
+                #die();
+        dd("it's alive !");
+    }
+    public function test(Request $request, $age) : Response
+    {
+        #we will use the http URL params with super _GLOBALS
+            #and this is to handle possible undifined value
+            /**
+             * $age = 0;
+             * if(!empty($_GET['age'])){
+             * $age = $_GET['age'];
+             * }
+             */
+        
+        #how to manage HTTP request with symfony/http-foudations
+            #==> $request = Request::createFromGlobals();
+            #this line can be passed as and argument in the main function like done up
+        dump($request);
+        # you can pass this variable as an argument directly in the function like up
+        # it's a form of destructuring
+        # ===> $age = $request->attributes->get('age');
+        #set responce using http foundation object class instanciation
+        return new Response("your age is: $age, little piece of shit !");
+    }
+}
